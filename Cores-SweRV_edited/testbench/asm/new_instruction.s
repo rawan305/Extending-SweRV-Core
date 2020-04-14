2224fc15b84623a16487290b2264dced51ab1211
nimi <nimrod.barazani@gmail.com>
2020-04-14 14:25:12 +0300

    new instruction test

---
+++ Cores-SweRV_edited/testbench/asm/new_instruction.s
@@ -0,0 +1,92 @@
+
+
+// Assembly code for Testing New Instruction
+
+
+#include "defines.h"
+
+#define STDOUT 0xd0580000
+
+
+// Code to execute
+.section .text
+.global _start
+_start:
+
+    // Clear minstret
+    csrw minstret, zero
+    csrw minstreth, zero
+
+    // Set up MTVEC - not expecting to use it though
+    li x1, RV_ICCM_SADR
+    csrw mtvec, x1
+
+
+    // Enable Caches in MRAC
+    li x1, 0x5f555555
+    csrw 0x7c0, x1
+
+    // Load string from hw_data
+    // and write to stdout address
+
+    li x3, STDOUT
+    la x4, hw_data
+
+loop:
+   lb x5, 0(x4)
+   sb x5, 0(x3)
+   addi x4, x4, 1
+   bnez x5, loop
+
+//~~~~~~~~~~~~~~~~~~~~new_part start ~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+   li x6,0x00 // result register
+   li x10, 0x00
+   li x11, 0x0A
+   // x10=0 start, x11=1 end : for (i=0 ; i < 10 ; i++)
+
+ext_loop:
+// first addi will be replaced with the new instruction
+   addi x6, x6, 1
+   li x3, STDOUT
+   la x4, message
+
+//int_loop will print the message below
+int_loop:
+   lb x5, 0(x4)
+   sb x5, 0(x3)
+   addi x4, x4, 1
+   bnez x5, int_loop
+   // print the result value
+   addi x7, x6, 0x30
+   sb x7, 0(x3)
+
+   //increment loop counter
+   addi x10, x10, 1
+   bne x10, x11, ext_loop
+
+//~~~~~~~~~~~~~~~~~~~~new_part end ~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+
+// Write 0xff to STDOUT for TB to terminate test.
+_finish:
+    li x3, STDOUT
+    addi x5, x0, 0xff
+    sb x5, 0(x3)
+    beq x0, x0, _finish
+.rept 100
+    nop
+.endr
+
+.global hw_data
+.data
+hw_data:
+.ascii "----------------------------------\n"
+.ascii "Hello World from SweRV EH1 @WDC !!\n"
+.ascii "----------------------------------\n"
+.byte 0
+.global message
+message:
+.ascii "\nNew instruction result: "
+.byte 0
